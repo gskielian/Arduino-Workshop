1b97c082629dd0c76f244da7f94599e0a034414f
gskielian <gregory.kielian@hackerdojo.com>
2013-09-14 17:47:36 -0700

    Create Blink.ino

---
+++ Blink.ino
@@ -0,0 +1,20 @@
+//red wire to 5 volts
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(13,HIGH);
+  Serial.println("pin 13 is high");
+  delay(1000);
+  digitalWrite(13,LOW);
+  Serial.println("pin 13 is low");
+  delay(1000);
+  
+}
+  
+  
